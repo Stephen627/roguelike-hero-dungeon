481c02fc955dc4d686b1d0644e88a3136f872acf
Stephen James <me@stephenjames.dev>
2022-02-23 12:28:59 +0000

    Add room prefabs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int level = 1;
+    public GameObject entryRoom;
+    public GameObject[] topDoors;
+    public GameObject[] bottomDoors;
+    public GameObject[] leftDoors;
+    public GameObject[] rightDoors;
+    public int maxDepth;
+    private List<GameObject> rooms;
 
-    void Start()
+    private void Start()
     {
-        
+        this.rooms = new List<GameObject>();
+        GameObject instance = Instantiate(this.entryRoom, new Vector3(0, 0, 0), Quaternion.identity);
+
+        this.rooms.Add(instance);
     }
 }
